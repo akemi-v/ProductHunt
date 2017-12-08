f1ffc0f2f02024cc6ed292231fc24031876b93ec
Maria <mari.semakova.a@gmail.com>
2017-12-09 01:53:49 +0300

    Add ProductsList- and ProductViewController

---
+++ ProductHunt/ProductsListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProductsListViewController.swift
+//  ProductHunt
+//
+//  Created by Maria Semakova on 12/9/17.
+//  Copyright © 2017 Maria Semakova. All rights reserved.
+//
+
+import UIKit
+
+class ProductsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
